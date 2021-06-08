bf912f7cef763ae346452ca82714fdbb5245ddf0
Rostislav <rost1sla4@mail.ru>
2021-06-08 18:27:33 +0300

    Webpack Config created

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./component/app.component";
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
